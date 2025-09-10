b7919735f2c65a20d14e077b29032da11b99d47b
nettemhandu <antonetteliebe@icloud.com>
2025-09-10 15:22:56 +0200

    implemented thanks for joining functionality.

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,13 +6,27 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        fullname = request.form["fullname"]
+        email = request.form["email"]
+        password = request.form["password"]
+        return redirect(url_for("events", user=fullname))
+    return render_template("signup.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        return redirect(url_for("events"))
     return render_template("signup.html")
 
 @app.route("/events")
 def events():
-    return render_template("events.html")
+    user = request.args.get("user")  # get the name from the URL
+    return render_template("events.html", user=user)
 
 if __name__ == "__main__":
     app.run(debug=True)
